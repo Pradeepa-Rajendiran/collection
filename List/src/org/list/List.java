6693145363067d61c38ba3b12fe0f92f053b7bf9
Pradeepa-Rajendiran <146409707+Pradeepa-Rajendiran@users.noreply.github.com>
2024-09-25 14:16:06 +0530

    Add files via upload

---
+++ List/src/org/list/List.java
@@ -0,0 +1,18 @@
+package org.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List {
+public static void main(String[] args) {
+	
+//	List methods
+	
+	List b=new ArrayList();
+	b.add(10);
+	
+	
+	
+}
+
+}
